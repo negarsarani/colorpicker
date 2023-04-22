4e882a24600f9b3c27ca3c2d99fe702dbc2700f1
Negar Sarani <negar.sarani2000@gmail.com>
2023-04-22 20:17:27 +0430

    create vite and files

---
+++ my-project/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-red-300'>salam</div>
+  )
+}
+
+export default App
